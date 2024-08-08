3650d49ef17a3669c77424ef78e606c2b3ad6131
Azmain Inquaid Haque <azmaininquaidhaque@gmail.com>
2024-08-08 08:12:53 +0600

    minor changes

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (() => {
     var __create = Object.create;
     var __defProp = Object.defineProperty;
@@ -33,16 +25,12 @@
         return to;
     };
     var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-        // If the importer is in node compatibility mode or this is not an ESM
-        // file that has been converted to a CommonJS file using a Babel-
-        // compatible transform (i.e. "__esModule" has not been set), then set
-        // "default" to the CommonJS "module.exports" for node compatibility.
+
         isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
         mod
     ));
     var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
-    // packages/shared/render/plugins/BaseSiteModules/tram-min.js
     var require_tram_min = __commonJS({
         "packages/shared/render/plugins/BaseSiteModules/tram-min.js"() {
             "use strict";
